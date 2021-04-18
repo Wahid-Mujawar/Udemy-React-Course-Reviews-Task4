e110d05f0f0ffad817719938a04bef2c8405ef87
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-04-18 16:25:20 +0530

    test

--- src/Review.js
+++ src/Review.js
@@ -3,7 +3,18 @@ import people from './Data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex]=useState(0);
+  const [image, name, job, text] = people(index);
+
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <image src={image} alt={name} class="people-img"/>
+        </div>
+      </article>
+    </main>
+  )
 };
 
 export default Review;
