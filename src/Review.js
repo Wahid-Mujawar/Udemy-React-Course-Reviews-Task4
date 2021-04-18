45c828a3993204d9abd34e91c3c2a84f3f4a829b
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-04-18 16:44:19 +0530

    test

--- src/Review.js
+++ src/Review.js
@@ -20,6 +20,15 @@ const Review = () => {
         <p className="job">{job}</p>
         <p className="info">{text}</p>
 
+        <div className="button-container">
+          <button className='next-btn'>
+            <FaChevronLeft/>
+          </button>
+          <button className='prev-btn'>
+            <FaChevronRight/>
+          </button>
+        </div>
+
       </article>
   );
 };
